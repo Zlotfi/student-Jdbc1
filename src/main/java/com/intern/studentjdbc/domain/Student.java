1c4c3161074dc8d8baaeadaa29539de769930597
victus <zlotfi758@gmail.com>
2024-12-25 14:15:49 +0330

    add attribute and constructor, getter and setter

--- src/main/java/com/intern/studentjdbc/domain/Student.java
+++ src/main/java/com/intern/studentjdbc/domain/Student.java
@@ -1,4 +1,67 @@
 package com.intern.studentjdbc.domain;
 
 public class Student {
+    private Integer studentId;
+    private String firstName;
+    private String lastName;
+    private String studentNumber;
+    private String userName;
+    private String password;
+
+    public Student(Integer studentId, String firstName, String lastName, String studentNumber, String userName, String password) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
