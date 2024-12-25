3acdb08b2ef161d0551a5a9d3d153ddc729c5a95
victus <zlotfi758@gmail.com>
2024-12-25 14:22:55 +0330

    add attribute and constructor , getter and setter method

--- src/main/java/com/intern/studentjdbc/domain/Course.java
+++ src/main/java/com/intern/studentjdbc/domain/Course.java
@@ -1,4 +1,28 @@
 package com.intern.studentjdbc.domain;
 
+@SuppressWarnings("unused")
 public class Course {
+    private Long courseId;
+    private String courseName;
+
+    public Course(Long courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 }
