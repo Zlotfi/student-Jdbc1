e7ff9bc5fc4cf96dc01fbfa8dc848034dfe6b833
victus <zlotfi758@gmail.com>
2024-12-25 14:41:49 +0330

    add CourseType attribute

--- src/main/java/com/intern/studentjdbc/domain/Course.java
+++ src/main/java/com/intern/studentjdbc/domain/Course.java
@@ -1,14 +1,18 @@
 package com.intern.studentjdbc.domain;
 
+import com.intern.studentjdbc.enumaration.CourseType;
+
 @SuppressWarnings("unused")
 //@Entity
 public class Course {
     private Long courseId;
     private String courseName;
+    private CourseType courseType;
 
-    public Course(Long courseId, String courseName) {
+    public Course(Long courseId, String courseName, CourseType courseType) {
         this.courseId = courseId;
         this.courseName = courseName;
+        this.courseType = courseType;
     }
 
     public Long getCourseId() {
@@ -25,5 +29,13 @@ public class Course {
 
     public void setCourseName(String courseName) {
         this.courseName = courseName;
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(CourseType courseType) {
+        this.courseType = courseType;
     }
 }
