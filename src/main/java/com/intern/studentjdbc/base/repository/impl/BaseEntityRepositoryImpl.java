221d7cb6ac5faf0ceb251477bc17f9e0d58a9c02
victus <zlotfi758@gmail.com>
2024-12-26 13:31:01 +0330

    add implement methods

--- src/main/java/com/intern/studentjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
+++ src/main/java/com/intern/studentjdbc/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -1,6 +1,42 @@
 package com.intern.studentjdbc.base.repository.impl;
 
+import com.intern.studentjdbc.base.domain.Entity;
 import com.intern.studentjdbc.base.repository.BaseEntityRepository;
 
-public abstract class BaseEntityRepositoryImpl implements BaseEntityRepository {
+public class BaseEntityRepositoryImpl
+        implements BaseEntityRepository {
+    @Override
+    public Entity[] findAll() {
+        return new Entity[0];
+    }
+
+    @Override
+    public Entity findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public Long count() {
+        return null;
+    }
+
+    @Override
+    public Entity save(Entity entity) {
+        return null;
+    }
+
+    @Override
+    public Entity update(Entity entity) {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return false;
+    }
 }
